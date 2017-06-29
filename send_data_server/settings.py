581cb9e37cae140b09d70fd1d0871dd7a578c0b8
lssromel <lssromel@hotmail.com>
2017-06-29 15:03:49 -0500

    Update settings.py

--- send_data_server/settings.py
+++ send_data_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=!mt@i=$)%&zdx%@35d(q7l928=-hz5%70$pb=+ef9q)whii!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
